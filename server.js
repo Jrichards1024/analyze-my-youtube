cdc678cca371ad14b529d439659ed4dd87ca4467
Jrichards1024 <jorichards@davidson.edu>
2023-04-13 14:43:09 -0400

    add normalizePort()

--- server.js
+++ server.js
@@ -275,4 +275,23 @@ app.listen(port,()=>{
     console.log(`app listening on port ${port}`);
 })
 
+/**
+ * Normalize a port into a number, string, or false
+ */
+function normalizePort(val){
+    var port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+
+    if (port >=0){
+        //port number
+        return port;
+    }
+
+    return false;
+}
+
 module.exports = app
